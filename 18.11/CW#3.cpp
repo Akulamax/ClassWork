968eb4cde02a8169847dd95f27d2f1ec7bb130d0
Akulamax <32465485+Akulamax@users.noreply.github.com>
2017-11-26 21:44:23 +0200

    Update CW#3.cpp

--- 18.11/CW#3.cpp
+++ 18.11/CW#3.cpp
@@ -1,23 +1,20 @@
-/*Напишите программу заполняющую двумерный массив размера N×N диагоналями, как показано в примере.*/
+/*Напишите программу заполняющую квадратный двумерный массив, как показано в примере.
+На главной диагонали стоят нули, а на остальных - удаление от главной диагонали.*/
 #include <iostream>
 #include <math.h>
 using namespace std;
 void dig_trench(int** A,int n){
 for (int i=0;i<n;i++){
+A[i] = new int[n];
 for (int j=0;j<n;j++){
-A[i][j]=n-sqrt((i-j)*(i-j));}
+A[i][j]=sqrt((i-j)*(i-j));}
 }
 }
+
 int main()
 {int n;
 cin>>n;
 int** Trench=new int *[n];
-for(int i = 0; i < n; i++) {
-Trench[i] = new int[n];
-for(int j = 0; j < n; j++) {
-Trench[i][j]=0;
-}
-}
 dig_trench(Trench,n);
 for (int i=0;i<n;i++){
 for (int j=0;j<n;j++){
@@ -25,7 +22,7 @@ cout<<Trench[i][j]<<" ";}
 for(int i=0;i<n;i=i+n)
 cout<<endl;
 }
- for (int i=0;i<n;i++)
+for (int i=0;i<n;i++)
 delete []Trench[i];
 delete []Trench;
 return 0;
